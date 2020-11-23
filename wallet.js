8dec9bb6180a11eb49e0366b1e0941b118504eaa
surinder singh <surinder83singh@gmail.com>
2020-11-23 06:50:35 +0530

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -7,18 +7,13 @@ const {RPC} = require('kaspa-module-node');
 
 bitcoreKaspaSetup();
 
+/*
 const rpc = new RPC({
     clientConfig:{
         host:"127.0.0.1:16210"
     }
-})
-const proto = rpc.client.proto;
-const methods = proto.KaspadMessage.type.field
-    .filter(({name})=>/request/i.test(name));//.map(f=>f.name)
-//console.log("methods", methods);//proto.KaspadMessage.type.field)
-rpc.disconnect();
+});
 
-/*
 Wallet.setRPC(rpc)
 */
 
@@ -36,8 +31,13 @@ program
 program
     .command('create')
     .description('Create Kaspa wallet')
-    .option('-p, --password <password>', "Password for wallet")
+    .requiredOption('-p, --password <password>', "Password for wallet")
     .action(async (cmd, options) => {
+        if(!cmd.password){
+            console.error("password is required")
+            cmd.outputHelp();
+            return;
+        }
         //console.log(Wallet)
         //console.log(cmd.password)
         const wallet = new Wallet();
@@ -51,8 +51,8 @@ program
     })
 
 program
-    .command('test-rpc')
-    .description('RPC test')
+    .command('run-grpc')
+    .description('Run gRPC "run -m <method> -j <json_data>" ')
     .option('-m, --method <method>', "rpc request, default will be 'getBlockDagInfoRequest' ")
     .option('-j, --json <json_data>', "rpc request args as json string, default will be '{}' ")
     .action(async (cmd, options) => {
@@ -60,41 +60,12 @@ program
         //console.log("method, json_data:", method, json_data)
         let args = JSON.parse(json_data)
         console.log("method, args:", method, args)
+
+        console.log("\nCalling:", method)
+        console.log("Arguments:\n", JSON.stringify(args, null, "  "))
         let result = await rpc.request(method, args);
-        console.log("Result:\n", result)
+        console.log("Result:\n", JSON.stringify(result, null, "  "))
         rpc.disconnect();
     })
-
-methods.slice(0, 2).forEach(method=>{
-    const {name, typeName} = method;
-    //console.log("method", method, proto[typeName])
-    let fields = proto[typeName].type.field;
-    //console.log("fields", fields)
-
-    let cmd = program.command(name).description(`RPC CALL ${name}`)
-    fields.forEach(f=>{
-        cmd.option(`--${f.name} <${f.name}>`, `Request argument ${f.name} of ${f.type}, default will be (${f.defaultValue}) `)
-    })
-        
-    cmd.action(async (cmd, options) => {
-        
-        let args = {};
-
-        fields.forEach(f=>{
-            if(cmd[f.name] !== undefined){
-                args[f.name] = cmd[f.name] || 1;
-            }
-        })
-
-        console.log("Calling:", name+"\n")
-        console.log("Arguments:", JSON.stringify(args, null, "  "))
-        let result = await rpc.request(name, args);
-        console.log("Result:\n", result)
-        rpc.disconnect();
-    })
-})
-
-
-
 
 program.parse(process.argv);
